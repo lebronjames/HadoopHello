06aa6a9f474d01771aca34893503d1b575ab9750
lebronjames <798258843@qq.com>
2017-12-13 16:44:53 +0800

    20171213

--- readme.txt
+++ readme.txt
@@ -95,7 +95,58 @@ bin/hadoop fs -rm -r /hadoop
 	bin/hadoop fs -put /usr/local/hadoop/hadoop_temp/2.txt /user/hadoop/input/
 	修改文件权限：bin/hadoop fs -chmod -R 777 /user/hadoop/input/
 	查看文件权限：bin/hadoop fs -ls /user/hadoop/input/
+8、测试期间关闭权限检查
+需要在namenode的hdfs-site.xml上，添加配置：(dfs.permissions.enabled -->false)
 
 五、代码开发
-1、
-2、
+public class HelloHDFS {
+	public static void main(String[] args) throws Exception {
+		//版本1
+//		URL url = new URL("http://www.baidu.com");
+//		InputStream is = url.openStream();
+//		IOUtils.copyBytes(is, System.out, 1024*1025, true);
+		//版本2
+//		URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+//		URL url = new URL("hdfs://asp-nj-srv98:9000/1.txt");
+//		InputStream is = url.openStream();
+//		IOUtils.copyBytes(is, System.out, 1024*1024, true);
+		//版本3
+		try {
+			Configuration conf = new Configuration();
+	        conf.set("fs.defaultFS", "hdfs://asp-nj-srv98:9000");
+	        conf.set("dfs.replication", "1");//默认为3
+	        FileSystem fileSystem = FileSystem.get(conf);
+	        
+			boolean success = fileSystem.mkdirs(new Path("/user/hadoop/input/sb"));
+			System.out.println("创建文件是否成功:" + success);
+			
+			success = fileSystem.exists(new Path("/user/hadoop/input/sb"));
+			System.out.println("文件是否存在:" + success);
+	        
+	        success = fileSystem.delete(new Path("/user/hadoop/input/sb"), true);
+	        System.out.println("删除文件是否成功：" + success);
+	        
+	        FSDataOutputStream out = fileSystem.create(new Path("/user/hadoop/input/test.data"));
+	        FileInputStream in = new FileInputStream("d:/test.txt");
+	        byte[] buf = new byte[4096];
+	        int len = in.read(buf);
+	        while(len != -1) {
+	            out.write(buf,0,len);
+	            len = in.read(buf);
+	        }
+	        in.close();
+	        out.close();
+	        
+	        FileStatus[] statuses = fileSystem.listStatus(new Path("/"));
+	        System.out.println("statuses.length:"+statuses.length);
+	        for(FileStatus status : statuses) {
+	        	System.out.println("status.getPath():"+status.getPath());
+	        	System.out.println("status.getPermission():"+status.getPermission());
+	        	System.out.println("status.getReplication()"+status.getReplication());
+	        }
+		}catch(Exception e) {
+			e.printStackTrace();
+		}  
+	}
+}
+
--- src/main/java/com/example/demo/HelloHDFS.java
+++ src/main/java/com/example/demo/HelloHDFS.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import java.io.FileInputStream;
+
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 public class HelloHDFS {
-	public static Log log =  LogFactory.getLog(HelloHDFS.class);
 	public static void main(String[] args) throws Exception {
 		//版本1
 //		URL url = new URL("http://www.baidu.com");
@@ -21,26 +22,39 @@ public class HelloHDFS {
 		//版本3
 		try {
 			Configuration conf = new Configuration();
-			conf.set("fs.defaultFS", "hdfs://asp-nj-srv98:9000");
-			conf.set("dfs.replication", "1");//默认为3
-			FileSystem fileSystem = FileSystem.get(conf);
-			
+	        conf.set("fs.defaultFS", "hdfs://asp-nj-srv98:9000");
+	        conf.set("dfs.replication", "1");//默认为3
+	        FileSystem fileSystem = FileSystem.get(conf);
+	        
 			boolean success = fileSystem.mkdirs(new Path("/user/hadoop/input/sb"));
-			log.info("创建文件是否成功:" + success);
+			System.out.println("创建文件是否成功:" + success);
 			
 			success = fileSystem.exists(new Path("/user/hadoop/input/sb"));
-	        log.info("文件是否存在:" + success);
+			System.out.println("文件是否存在:" + success);
 	        
 	        success = fileSystem.delete(new Path("/user/hadoop/input/sb"), true);
-	        log.info("删除文件是否成功：" + success);
+	        System.out.println("删除文件是否成功：" + success);
+	        
+	        FSDataOutputStream out = fileSystem.create(new Path("/user/hadoop/input/test.data"));
+	        FileInputStream in = new FileInputStream("d:/test.txt");
+	        byte[] buf = new byte[4096];
+	        int len = in.read(buf);
+	        while(len != -1) {
+	            out.write(buf,0,len);
+	            len = in.read(buf);
+	        }
+	        in.close();
+	        out.close();
+	        
+	        FileStatus[] statuses = fileSystem.listStatus(new Path("/"));
+	        System.out.println("statuses.length:"+statuses.length);
+	        for(FileStatus status : statuses) {
+	        	System.out.println("status.getPath():"+status.getPath());
+	        	System.out.println("status.getPermission():"+status.getPermission());
+	        	System.out.println("status.getReplication()"+status.getReplication());
+	        }
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
-		
-        
-        
 	}
-	
-	
-	
 }
